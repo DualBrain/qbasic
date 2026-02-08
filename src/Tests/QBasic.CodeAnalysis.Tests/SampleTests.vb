0c52e34d1d2cbae3f1573eafb2c65d836b693341
Cory Smith <dualbrain@msn.com>
2026-02-07 23:05:52 -0600

    Modified EXTERR unit tests.

--- src/Tests/QBasic.CodeAnalysis.Tests/SampleTests.vb
+++ src/Tests/QBasic.CodeAnalysis.Tests/SampleTests.vb
@@ -1646,11 +1646,42 @@ PRINT EXP(X-1)
 
       ' Name: EXTERR (1)
 
+      '# EXTERR Function
+      '
+      '## Purpose:
+      '
+      'To return extended error information.
+      '
+      '## Syntax:
+      '
+      '  `EXTERR(n)`
+      '
+      '## Comments:
+      '`EXTERR` returns "extended" error information provided by versions of DOS 3.0 and greater. For 
+      'versions of DOS earlier than 3.0, `EXTERR` always returns zero. The single integer argument must 
+      'be in the range 0-3 as follows:
+      '
+      '| Value of *n* | Return Value |
+      '| ------------ | ------------ |
+      '| 0            | Extended error code |
+      '| 1            | Extended error class |
+      '| 2            | Extended error suggested action |
+      '| 3            | Extended error locus |
+      '
+      'The values returned are not defined by GW-BASIC, but by DOS. Refer to the *MS-DOS Programmer's Reference*
+      '(version 3.0 or later) for a description of the values returned by the DOS extended error function.
+      '
+      'The extended error code is actually retrieved and saved by GW-BASIC each time appropriate 
+      'DOS functions are performed. Thus when an `EXTERR` function call is made, these saved values 
+      'are returned.
+      '
+      ' NOTE: Does not appear to be in QBasic
+
       Dim sample = "
 PRINT EXTERR(-1)
 "
 
-      Dim expected = "Illegal function call line 1"
+      Dim expected = "Subscript out of range in 1"
 
       Dim eval = Evaluate(sample)
       Dim result = eval.Result
@@ -1666,11 +1697,14 @@ PRINT EXTERR(-1)
 
       ' Name: EXTERR (2)
 
+      ' NOTE: See above.
+
       Dim sample = "
 PRINT EXTERR(4)
 "
 
-      Dim expected = "Illegal function call line 1"
+      'Dim expected = "Illegal function call line 1"
+      Dim expected = "0" ' Looking for 0 since that is what QBasic is doing in this example.
 
       Dim eval = Evaluate(sample)
       Dim result = eval.Result
@@ -1889,7 +1923,7 @@ Handler:
   END
 "
 
-      Dim expected = "FOR Without NEXT line 2"
+      Dim expected = "Error: Error 26: FOR Without NEXT"
 
       Dim eval = Evaluate(sample)
       Dim result = eval.Result
